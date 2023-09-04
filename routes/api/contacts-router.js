37e5fa83e03a04a3c5fc25c140eba6e042613c88
Andrii Tsokur <tsokur@outlook.com>
2023-09-04 23:58:43 +0300

    feat: Adds complete code for HW02

--- routes/api/contacts-router.js
+++ routes/api/contacts-router.js
@@ -1,25 +1,81 @@
 import express from 'express';
+import Joi from 'joi';
+
+import { contactsService } from '../../models/contacts/index.js';
+import { HttpError } from '../../helpers/index.js';
 
 const contactsRouter = express.Router();
 
-contactsRouter.get('/', async (req, res, next) => {
-	res.json({ message: 'template message' });
+const contactTemplate = Joi.object({
+	name: Joi.string().required(),
+	email: Joi.string().required(),
+	phone: Joi.string().required(),
+});
+
+contactsRouter.get('/', async (_, res, next) => {
+	try {
+		const data = await contactsService.listContacts();
+		res.json(data);
+	} catch (error) {
+		next(error);
+	}
 });
 
 contactsRouter.get('/:contactId', async (req, res, next) => {
-	res.json({ message: 'template message' });
+	try {
+		const { contactId: id } = req.params;
+		const data = await contactsService.getContactById(id);
+		if (!data) {
+			throw HttpError(404, `No contact found for ID: ${id}`);
+		}
+		res.json(data);
+	} catch (error) {
+		next(error);
+	}
 });
 
 contactsRouter.post('/', async (req, res, next) => {
-	res.json({ message: 'template message' });
+	try {
+		const { error } = contactTemplate.validate(req.body);
+		if (error) {
+			throw HttpError(400, error.message);
+		}
+		const data = await contactsService.addContact(req.body);
+		res.status(201).json(data);
+	} catch (error) {
+		next(error);
+	}
 });
 
 contactsRouter.delete('/:contactId', async (req, res, next) => {
-	res.json({ message: 'template message' });
+	try {
+		const { contactId: id } = req.params;
+		const data = await contactsService.removeContact(id);
+		if (!data) {
+			throw HttpError(404, `No contact found for ID: ${id}`);
+		}
+		res.json({ message: 'Contact deleted' });
+	} catch (error) {
+		next(error);
+	}
 });
 
 contactsRouter.put('/:contactId', async (req, res, next) => {
-	res.json({ message: 'template message' });
+	try {
+		const { error } = contactTemplate.validate(req.body);
+		if (error) {
+			throw HttpError(400, error.message);
+		}
+
+		const { contactId: id } = req.params;
+		const data = await contactsService.updateContact(id, req.body);
+		if (!data) {
+			throw HttpError(404, `No contact found for ID: ${id}`);
+		}
+		res.json(data);
+	} catch (error) {
+		next(error);
+	}
 });
 
 export default contactsRouter;
