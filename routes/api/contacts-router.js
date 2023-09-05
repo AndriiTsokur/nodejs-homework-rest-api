5ebda0f8211c8c1a8aa9eaf244f99f5ba28e0749
Andrii Tsokur <tsokur@outlook.com>
2023-09-05 16:21:59 +0300

    fix: Hardcore for pervs

--- routes/api/contacts-router.js
+++ routes/api/contacts-router.js
@@ -1,81 +1,25 @@
 import express from 'express';
-import Joi from 'joi';
-
-import { contactsService } from '../../models/contacts/index.js';
-import { HttpError } from '../../helpers/index.js';
+import contactsController from '../../controllers/contacts-controller.js';
+import contactValidation from '../../middleware/validation/contact-validation.js';
 
 const contactsRouter = express.Router();
 
-const contactTemplate = Joi.object({
-	name: Joi.string().required(),
-	email: Joi.string().required(),
-	phone: Joi.string().required(),
-});
+contactsRouter.get('/', contactsController.getAll);
 
-contactsRouter.get('/', async (_, res, next) => {
-	try {
-		const data = await contactsService.listContacts();
-		res.json(data);
-	} catch (error) {
-		next(error);
-	}
-});
+contactsRouter.get('/:contactId', contactsController.getById);
 
-contactsRouter.get('/:contactId', async (req, res, next) => {
-	try {
-		const { contactId: id } = req.params;
-		const data = await contactsService.getContactById(id);
-		if (!data) {
-			throw HttpError(404, `No contact found for ID: ${id}`);
-		}
-		res.json(data);
-	} catch (error) {
-		next(error);
-	}
-});
+contactsRouter.post(
+	'/',
+	contactValidation.addContactValidate,
+	contactsController.add
+);
 
-contactsRouter.post('/', async (req, res, next) => {
-	try {
-		const { error } = contactTemplate.validate(req.body);
-		if (error) {
-			throw HttpError(400, error.message);
-		}
-		const data = await contactsService.addContact(req.body);
-		res.status(201).json(data);
-	} catch (error) {
-		next(error);
-	}
-});
+contactsRouter.delete('/:contactId', contactsController.deleteById);
 
-contactsRouter.delete('/:contactId', async (req, res, next) => {
-	try {
-		const { contactId: id } = req.params;
-		const data = await contactsService.removeContact(id);
-		if (!data) {
-			throw HttpError(404, `No contact found for ID: ${id}`);
-		}
-		res.json({ message: 'Contact deleted' });
-	} catch (error) {
-		next(error);
-	}
-});
-
-contactsRouter.put('/:contactId', async (req, res, next) => {
-	try {
-		const { error } = contactTemplate.validate(req.body);
-		if (error) {
-			throw HttpError(400, error.message);
-		}
-
-		const { contactId: id } = req.params;
-		const data = await contactsService.updateContact(id, req.body);
-		if (!data) {
-			throw HttpError(404, `No contact found for ID: ${id}`);
-		}
-		res.json(data);
-	} catch (error) {
-		next(error);
-	}
-});
+contactsRouter.put(
+	'/:contactId',
+	contactValidation.addContactValidate,
+	contactsController.updateById
+);
 
 export default contactsRouter;
