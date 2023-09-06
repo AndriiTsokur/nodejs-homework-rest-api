dcfb3f5e2af22d5dfa443e4b21e2c28a0d5e13b7
Andrii Tsokur <tsokur@outlook.com>
2023-09-06 19:19:02 +0300

    feat: Adds MongoDB init

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -1,51 +1,52 @@
-import { contactsService } from '../models/contacts/index.js';
+// import { contactsService } from '../models/contacts/index.js';
 import { HttpError } from '../helpers/index.js';
 import { ctrlWrapper } from '../decorators/index.js';
+import Contact from '../models/Contact.js';
 
 const getAll = async (_, res) => {
-	const data = await contactsService.listContacts();
+	const data = await Contact.find();
 	res.json(data);
 };
 
-const getById = async (req, res) => {
-	const { contactId: id } = req.params;
+// const getById = async (req, res) => {
+// 	const { contactId: id } = req.params;
 
-	const data = await contactsService.getContactById(id);
-	if (!data) {
-		throw HttpError(404, `No contact found for ID: ${id}`);
-	}
-	res.json(data);
-};
+// 	const data = await contactsService.getContactById(id);
+// 	if (!data) {
+// 		throw HttpError(404, `No contact found for ID: ${id}`);
+// 	}
+// 	res.json(data);
+// };
 
-const add = async (req, res) => {
-	const data = await contactsService.addContact(req.body);
-	res.status(201).json(data);
-};
+// const add = async (req, res) => {
+// 	const data = await contactsService.addContact(req.body);
+// 	res.status(201).json(data);
+// };
 
-const deleteById = async (req, res) => {
-	const { contactId: id } = req.params;
+// const deleteById = async (req, res) => {
+// 	const { contactId: id } = req.params;
 
-	const data = await contactsService.removeContact(id);
-	if (!data) {
-		throw HttpError(404, `No contact found for ID: ${id}`);
-	}
-	res.json({ message: 'Contact deleted' });
-};
+// 	const data = await contactsService.removeContact(id);
+// 	if (!data) {
+// 		throw HttpError(404, `No contact found for ID: ${id}`);
+// 	}
+// 	res.json({ message: 'Contact deleted' });
+// };
 
-const updateById = async (req, res) => {
-	const { contactId: id } = req.params;
-	const data = await contactsService.updateContact(id, req.body);
+// const updateById = async (req, res) => {
+// 	const { contactId: id } = req.params;
+// 	const data = await contactsService.updateContact(id, req.body);
 
-	if (!data) {
-		throw HttpError(404, `No contact found for ID: ${id}`);
-	}
-	res.json(data);
-};
+// 	if (!data) {
+// 		throw HttpError(404, `No contact found for ID: ${id}`);
+// 	}
+// 	res.json(data);
+// };
 
 export default {
 	getAll: ctrlWrapper(getAll),
-	getById: ctrlWrapper(getById),
-	add: ctrlWrapper(add),
-	deleteById: ctrlWrapper(deleteById),
-	updateById: ctrlWrapper(updateById),
+	// getById: ctrlWrapper(getById),
+	// add: ctrlWrapper(add),
+	// deleteById: ctrlWrapper(deleteById),
+	// updateById: ctrlWrapper(updateById),
 };
--- routes/api/contacts-router.js
+++ routes/api/contacts-router.js
@@ -6,20 +6,20 @@ const contactsRouter = express.Router();
 
 contactsRouter.get('/', contactsController.getAll);
 
-contactsRouter.get('/:contactId', contactsController.getById);
+// contactsRouter.get('/:contactId', contactsController.getById);
 
-contactsRouter.post(
-	'/',
-	contactValidation.addContactValidate,
-	contactsController.add
-);
+// contactsRouter.post(
+// 	'/',
+// 	contactValidation.addContactValidate,
+// 	contactsController.add
+// );
 
-contactsRouter.delete('/:contactId', contactsController.deleteById);
+// contactsRouter.delete('/:contactId', contactsController.deleteById);
 
-contactsRouter.put(
-	'/:contactId',
-	contactValidation.addContactValidate,
-	contactsController.updateById
-);
+// contactsRouter.put(
+// 	'/:contactId',
+// 	contactValidation.addContactValidate,
+// 	contactsController.updateById
+// );
 
 export default contactsRouter;
